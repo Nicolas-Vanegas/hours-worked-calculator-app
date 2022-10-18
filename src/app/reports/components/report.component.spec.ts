33e350e6329571104aceb712627b9224c919ed98
Nicolas Vanegas <nicolas.vanegas.p@hotmail.com>
2022-10-18 14:45:54 -0500

    cambiar nombre. arreglar tests

--- src/app/reports/components/report.component.spec.ts
+++ src/app/reports/components/report.component.spec.ts
@@ -1,22 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ReporteComponent } from './report.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReportComponent } from './report.component';
+import { ReportService } from '../services/report.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('ReporteComponent', () => {
-  let component: ReporteComponent;
-  let fixture: ComponentFixture<ReporteComponent>;
+  let component: ReportComponent;
+  let fixture: ComponentFixture<ReportComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ReporteComponent],
+      declarations: [ReportComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, FormsModule],
+      providers: [ReportService],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ReporteComponent);
+    fixture = TestBed.createComponent(ReportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(ReportComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Reporte de Servicios brindados'
+    );
   });
 });
